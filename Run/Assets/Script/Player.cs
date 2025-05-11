cb9f95baa2f9926654e91ba23bf1da816f076bfd
グエンカインアン <96474826+ChoiKanghee@users.noreply.github.com>
2025-05-11 17:01:58 +0700

    update code

--- Run/Assets/Script/Player.cs
+++ Run/Assets/Script/Player.cs
@@ -8,18 +8,27 @@ public class Player : MonoBehaviour
 
     private Animator anim;
 
-
     [Header("Movement Info")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
 
-    private bool pllayerUnlocked;
+    private bool playerUnlocked;
+    private bool canDoubleJump;
 
+    [Header("Slide Info")]
+    [SerializeField] private float slideSpeed;
+    [SerializeField] private float slideTime;
+    private float slideTimeCounter;
+    private bool isSliding;
 
     [Header("Collision Info")]
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private Vector2 wallCheckSize;
     private bool isGrounded;
+    private bool wallDetected;
 
     // Start is called before the first frame update
     void Start()
@@ -32,39 +41,90 @@ public class Player : MonoBehaviour
     void Update()
     {
         AnimatorControllers();
-
-        if (pllayerUnlocked)
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-
-        
         CheckCollision();
+
+        slideTimeCounter -= Time.deltaTime;
+
+        if (playerUnlocked && !wallDetected)
+            Movement();
+
+
+        if (isGrounded)
+            canDoubleJump = true;
+
+        CheckForSlide();
         CheckInput();
 
     }
 
+    private void CheckForSlide()
+    {
+        if (slideTimeCounter < 0;)
+        {
+            isSliding = false;
+        }
+
+    private void Movement()
+    {
+        if (isSliding)
+            rb.velocity = new Vector2(slideSpeed, rb.velocity.y);
+
+        else
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+    }
+
+
     private void AnimatorControllers()
     {
-        anim.SetBool("isGrounded", isGrounded);
         anim.SetFloat("xVelocity", rb.velocity.x);
         anim.SetFloat("yVelocity", rb.velocity.y);
+
+        anim.SetBool("canDoubleJump", canDoubleJump);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isSliding", isSliding);
     }
 
     private void CheckCollision()
     {
         isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
         Debug.Log("Player Grounded: " + isGrounded);
+        wallDetected = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.right, 0, whatIsGround);
     }
 
     private void CheckInput()
     {
         if (Input.GetButtonDown("Fire2"))
-            pllayerUnlocked = true;
+            playerUnlocked = true;
         Debug.Log("Player Run");
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump"))
+            JumpButton();
+        //Debug.Log("Player Jump");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            SlideButton();
+        //Debug.Log("Player Slide");
+    }
+
+    private void SlideButton()
+    {
+            isSliding = true;
+            slideTimeCounter = slideTime;
+    }
+
+    private void JumpButton()
+    {
+        if (isGrounded)
+        {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        Debug.Log("Player Jump");
+            Debug.Log("Player Double Jump");
+        }
+        else if (canDoubleJump)
+        {
+            canDoubleJump = false;
+            rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
+            Debug.Log("Player Jump");
+        }
     }
 
     private void OnDrawGizmos()
@@ -72,6 +132,7 @@ public class Player : MonoBehaviour
         //Gizmos.color = Color.red;
         //Gizmos.DrawRay(transform.position, Vector2.down * groundCheckDistance);
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+        Gizmos.DrawWireCube(wallCheck.position, wallCheckSize);
     }
 
 }
